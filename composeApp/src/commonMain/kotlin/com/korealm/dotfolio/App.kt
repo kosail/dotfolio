e73aaba3087cf9bb00ce481affe7f5735cd01fd3
kosail <dsolishn@pm.me>
2025-06-12 13:34:31 -0600

    updated icons and finished mockup of taskbar
    There is still no interactiveness at all, but visually it starting to look how I want it to be.
    
    Note: The weather is hardcoded, and the time and date have a bug in which they don't update correctly

--- composeApp/src/commonMain/kotlin/com/korealm/dotfolio/App.kt
+++ composeApp/src/commonMain/kotlin/com/korealm/dotfolio/App.kt
@@ -4,11 +4,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.korealm.dotfolio.state.rememberAppThemeState
 import com.korealm.dotfolio.ui.DesktopEnvironment
 import com.korealm.dotfolio.ui.DesktopShortcuts
 import com.korealm.dotfolio.ui.theme.MicaTheme
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
@@ -16,6 +23,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     val themeState = rememberAppThemeState()
+    var localDateTime by remember { mutableStateOf(Pair<String, String>("", "")) }
 
     MicaTheme(
         darkTheme = themeState.isDarkTheme
@@ -24,8 +32,12 @@ fun App() {
             modifier = Modifier
                 .safeContentPadding()
                 .fillMaxSize(),
-        ){
-            DesktopEnvironment(themeState, modifier = Modifier)
+        ) {
+            DesktopEnvironment(
+                clock = localDateTime,
+                onClockUpdate = { time, date -> localDateTime = localDateTime.copy(time, date) },
+                themeState = themeState,
+                modifier = Modifier)
             DesktopShortcuts(modifier = Modifier.fillMaxSize())
         }
     }
