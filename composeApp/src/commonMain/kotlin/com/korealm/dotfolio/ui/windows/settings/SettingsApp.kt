89c1f1eaf86b83f6e6a43f1a1aef5cafd3657481
kosail <kamururi@protonmail.com>
2025-06-19 16:30:11 -0600

    Added dark mode switch, dev mode switch and early implementation of background switcher
    The background switcher will work by hoisting the wallpaper into ThemeState.
    
    What I have no idea how to do, is to trigger that change with animation of fadeIn and fadeOut without using an AnimatedVisibility. I'll have to research on that

--- composeApp/src/commonMain/kotlin/com/korealm/dotfolio/ui/windows/settings/SettingsApp.kt
+++ composeApp/src/commonMain/kotlin/com/korealm/dotfolio/ui/windows/settings/SettingsApp.kt
@@ -3,6 +3,7 @@ package com.korealm.dotfolio.ui.windows.settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.DpSize
@@ -21,18 +22,21 @@ fun SettingsApp(
 ): WindowApp {
     // isSelected is to handle selected menu options
     var selectedIndex by remember { mutableIntStateOf(0) }
+    var isDevModeOn by remember { mutableStateOf(false) }
 
     return WindowApp(
         appId = "settings",
         title = stringResource(Res.string.settings),
         icon = painterResource(Res.drawable.settings),
-        defaultSize = DpSize(1000.dp, 500.dp),
+        defaultSize = DpSize(950.dp, 500.dp),
         titleBar = { SettingsTitleBar(themeState, onClose) },
         content = {
             SettingsWindowContent(
                 themeState = themeState,
+                isDevModeOn = isDevModeOn,
                 selectedIndex = selectedIndex,
                 onSelectIndex = { selectedIndex = it },
+                onDevModeChange = { isDevModeOn = !isDevModeOn }
             )
         }
     )
