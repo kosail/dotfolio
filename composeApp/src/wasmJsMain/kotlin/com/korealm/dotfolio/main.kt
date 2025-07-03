535d6cdb6be28dbe254e5ab6a20518ac5b84b13e
kosail <dsolishn@pm.me>
2025-07-03 07:51:42 -0600

    Broken WASM

--- composeApp/src/wasmJsMain/kotlin/com/korealm/dotfolio/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/korealm/dotfolio/main.kt
@@ -4,11 +4,10 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.w3c.dom.HTMLAudioElement
 
 @OptIn(
     ExperimentalComposeUiApi::class,
-//    ExperimentalResourceApi::class // TODO: I may require it for font loading. I'll check on that later
+    ExperimentalResourceApi::class // TODO: I may require it for font loading. I'll check on that later
 )
 fun main() {
     ComposeViewport(document.body!!) {
